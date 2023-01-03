5d9da4550fe51d21c9c37a3f637c19a5dfc7e993
Hyuk Jin Kwon <hkwon2054@conestogac.on.go>
2023-01-03 16:21:34 -0500

    task and thread project

---
+++ Task/a04/FileIO.cs
@@ -0,0 +1,152 @@
+﻿/*
+* DESCRIPTION		:
+* 	It's a class that summarizes file-related methods
+* 	It has the ability to create and check files and add arbitrary characters
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace a04
+{
+    internal class FileIO
+    {
+        // Properties
+        public static Mutex mutex = new Mutex();
+        public static bool done = false;
+        private string path { get; set; }
+        private int size { get; set; }
+
+        // Constructor
+        public FileIO()
+        { }
+
+        // Constructor
+        public FileIO(string fPath, int fSize)
+        {
+            this.path = fPath;
+            this.size = fSize;
+        }
+
+        /*
+         * Method       : CreateFile()
+         * Description  : Create a file in the forwarded path
+         * Parameters   : string fPath : file path
+         * Return       : void
+         */
+        public void CreateFile(string fPath)
+        {
+            try
+            {
+                StreamWriter sw = File.CreateText(fPath);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+            
+        }
+
+        /*
+         * Method       : CheckFileExists()
+         * Description  : Check the file exists
+         * Parameters   : string fPath : file path
+         * Return       : void
+         */
+        public bool CheckFileExists(string fPath)
+        {
+            try
+            {
+                if (File.Exists(fPath))
+                {
+                    Console.WriteLine("The file exists. Do you want to overwrite it?");
+                    Console.WriteLine("If you want overwrite, Please press Y");
+                    // Check the file is overwritten
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.KeyChar == 'Y' || key.KeyChar == 'y')
+                    {
+                        CreateFile(fPath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Same file exists");
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+            
+            return false;
+        }
+
+        /*
+         * Method       : WriteRandomData()
+         * Description  : Add random data to the file
+         * Parameters   : none
+         * Return       : void
+         */
+        public void WriteRandomData()
+        {
+            while (!done)
+            {
+                mutex.WaitOne();
+                try
+                {
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(Guid.NewGuid().ToString());
+                        sw.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.ToString());
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+
+        /*
+         * Method       : CheckFileSize()
+         * Description  : Check the file size every 0.5 seconds
+         *              : Exits the program when the file size is larger than the size you specify
+         * Parameters   : none
+         * Return       : void
+         */
+        public void CheckFileSize()
+        {
+            while (!done)
+            {
+                try
+                {
+                    long length = new FileInfo(path).Length;
+                    Console.WriteLine("File size: " + length + " bytes");
+                    if (length >= size)
+                    {
+                        done = true;
+                    }
+                    Thread.Sleep(500);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.ToString());
+                }
+            }
+
+            long finalLength = new FileInfo(path).Length;
+            Console.WriteLine("Final size: " + path + " " + finalLength + " bytes");
+        }
+    }
+}
---
+++ Task/a04/Program.cs
@@ -0,0 +1,128 @@
+﻿/*
+* DESCRIPTION		:
+* 	A program that get two args (file path, and file size)
+* 	Creates a file and runs a TASK up to the file size you specify
+* 	25 TASKs are set to meet requirements
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace a04
+{
+    internal class Program
+    {
+        // Constants
+        public const string HELP = "Usage: Write <full path and file name> <size of file to be create>\n" +
+                            "---- ex, [a04.exe C:/test/test.txt 100000] ----";
+        public const int TASKS = 25;
+
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            // Display Usage
+            if (args[0] == "/?")
+            {
+                Console.WriteLine(HELP);
+                return;
+            }
+            // check the exact args number
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect arguments");
+                Console.WriteLine("Please use /? for help");
+                return;
+            }            
+
+            int size;
+            bool sizeCheck = int.TryParse(args[1], out size);
+            // Check the minimum, maximum sizes and integer
+            if (sizeCheck)
+            {
+                if (size < 1000 || size > 20000000)
+                {
+                    Console.WriteLine("The minimum size is 1,000, and the maximum size is 20,000,000");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("File size must be a valid integer");
+                return;
+            }
+
+            string filename = Path.GetFileName(path);
+            string directory = Path.GetDirectoryName(path);
+            FileIO fio = new FileIO(path, size);
+            // Check the path entered by the user is correct
+            if (Path.IsPathRooted(path))
+            {
+                // Check the file name entered by the user is correct
+                if (filename == null || filename == "")
+                {
+                    Console.WriteLine("Missing filename");
+                    return ;
+                }
+                else
+                {
+                    // Check the path of the user exists and create a file
+                    try
+                    {
+                        if (!Directory.Exists(directory))
+                        {
+                            Directory.CreateDirectory(directory);
+                            fio.CreateFile(path);
+                        }
+                        else
+                        {
+                            if (!fio.CheckFileExists(path))
+                            {
+                                fio.CreateFile(path);
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception: " + ex.ToString());
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(HELP);
+                return ;
+            }
+
+            Task[] tasks = new Task[TASKS];
+            Task checkSize = new Task(fio.CheckFileSize);
+            // Run File Size Check Task
+            checkSize.Start();      
+            // Run All Tasks
+            try
+            {
+                for (int i = 0; i < tasks.Length; i++)
+                {
+                    tasks[i] = new Task(fio.WriteRandomData);
+                    tasks[i].Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+
+
+            Console.ReadKey();                        
+        }       
+    }
+}
